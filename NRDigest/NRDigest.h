72556e83fb18a128c9618c6727dd5e47c31c5dcd
Nikolai Ruhe <nikolai@flightrecorderapp.com>
2013-12-09 10:36:40 +0100

    Initial Commit

---
+++ NRDigest/NRDigest.h
@@ -0,0 +1,13 @@
+//
+//  NRDigest.h
+//  NRDigest
+//
+//  Created by Nikolai Ruhe on 09.12.13.
+//  Copyright (c) 2013 Nikolai Ruhe. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NRDigest : NSObject
+
+@end
---
+++ NRDigest/NRDigest.m
@@ -0,0 +1,13 @@
+//
+//  NRDigest.m
+//  NRDigest
+//
+//  Created by Nikolai Ruhe on 09.12.13.
+//  Copyright (c) 2013 Nikolai Ruhe. All rights reserved.
+//
+
+#import "NRDigest.h"
+
+@implementation NRDigest
+
+@end
